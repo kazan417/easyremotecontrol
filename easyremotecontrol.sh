9deec70857ddac69213f757a55b28607d2482be5
Mikhail Valerievich Kazantsev <kazan417@mail.ru>
2024-03-13 00:36:38 +0700

    initial commit

---
+++ easyremotecontrol.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+###автор Казанцев Михаил Валеьевич (kazan417@mail.ru) лицензия MIT
+
+if [ -n "$(command -v yum)" ]; then
+  if [[ $EUID -ne 0 ]]; then
+     echo "введите пароль суперпользователя"
+     su -c 	"/bin/bash ./easyremotecontrol.sh"
+     exit 0
+  fi
+  if [[ $EUID -ne 0 ]]; then
+  echo "ошибка получения прав суперпользователя"
+  exit 1
+  fi
+echo "скрипт настройки удаленного доступа"
+echo "введите желаемый пароль для удаленного доступа"
+read mypass  
+echo "установка x11vnc"
+yum install x11vnc -y
+echo "прописываем файл автозагрузки"
+echo "
+[Unit] 
+Description=Start x11vnc at startup. 
+After=multi-user.target 
+
+[Service] 
+Type=simple 
+ExecStart=/usr/bin/x11vnc -auth guess -forever -loop -noxdamage -repeat -rfbport 5900 -shared -dontdisconnect -rfbauth /etc/vncpasswd -o /var/log/x11vnc.log
+
+[Install]
+WantedBy=multi-user.target
+" > /lib/systemd/system/x11vnc.service
+x11vnc -storepasswd "$mypass" /etc/vncpasswd
+echo "включаем автозагрузку vncserver"
+systemctl enable x11vnc.service
+echo "запускаем vncserver"
+systemctl start x11vnc.service
+echo "удаленный доступ настроен"
+read
+fi
+if [ -n "$(command -v apt-get)" ]; then
+	echo "найден apt-get устанавливаем требуемые программы с помощью apt-get"
+  if [[ $EUID -ne 0 ]]; then
+     echo "введите пароль суперпользователя"
+     su -c 	"/bin/bash ./easyremotecontrol.sh"
+     exit 0
+  fi
+  if [[ $EUID -ne 0 ]]; then
+  echo "ошибка получения прав суперпользователя"
+  exit 1
+  fi
+echo "установка x11vnc"
+sudo apt-get install x11vnc -y
+x11vnc -storepasswd "$mypass" /etc/vncpasswd
+echo "
+[Unit]
+Description=Start x11vnc at startup.
+After=multi-user.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/x11vnc -auth guess -forever -loop -noxdamage -repeat -rfbport 5900 -shared -dontdisconnect -rfbauth /etc/vncpasswd -o /var/log/x11vnc.log
+
+[Install]
+WantedBy=multi-user.target
+" > /lib/systemd/system/x11vnc.service
+x11vnc -storepasswd "$mypass" /etc/vncpasswd
+echo "включаем автозагрузку vncserver"
+systemctl enable x11vnc.service
+echo "запускаем vncserver"
+systemctl start x11vnc.service
+echo "удаленный доступ настроен"
+read
+fi
+
