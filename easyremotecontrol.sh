dea13bcf5a958eaa7cb213be43be626f81ef1f60
kazan417 <kazan417@mail.ru>
2024-06-27 22:08:12 +0700

    code refactoring
    fixed by astra community

--- easyremotecontrol.sh
+++ easyremotecontrol.sh
@@ -1,69 +1,61 @@
-#!/bin/bash
 ###автор Казанцев Михаил Валеьевич (kazan417@mail.ru) лицензия MIT
-echo "скрипт настройки удаленного доступа"
-echo "введите желаемый пароль для удаленного доступа"
-read mypass  
-if [ -n "$(command -v yum)" ]; then
-  if [[ $EUID -ne 0 ]]; then
-     echo "введите пароль суперпользователя"
-     su -c 	"/bin/bash ./easyremotecontrol.sh"
-     exit 0
+SYSTEMD="/etc/systemd/system/x11vnc.service"
+PACK="x11vnc"
+PASS="/etc/vncpasswd"
+PORT="5900"
+
+function main() {
+  if [[ "$EUID" -ne 0 ]]; then
+    printf "%s\\n" "Недостаточно прав! Запустите скрипт через sudo или от root"
+    exit 1
   fi
-  if [[ $EUID -ne 0 ]]; then
-  echo "ошибка получения прав суперпользователя"
-  exit 1
+}
+
+function install() {
+  if [[ $(which apt) ]]; then
+      apt-get install ${PACK} -y
+  elif [[ $(which yum) ]]; then
+      yum install ${PACK} -y
   fi
-echo "установка x11vnc"
-yum install x11vnc -y
-echo "прописываем файл автозагрузки"
-echo "
+
+  if [[ $? == 0 ]]; then
+      clear
+      printf "\n%s\n" "Установка ${PACK}: [OK]"
+  else
+      printf "\n%s\n" "Установка ${PACK}: [FAIL]"
+      exit 1
+  fi
+}
+
+function settings() {
+  printf "\n%s:\n" "Введите пароль для удаленного доступа"
+  read mypass
+  x11vnc -storepasswd "$mypass" ${PASS}
+
+  cat > ${SYSTEMD} <<EOF
 [Unit] 
 Description=Start x11vnc at startup. 
 After=multi-user.target 
 
 [Service] 
 Type=simple 
-ExecStart=/usr/bin/x11vnc -auth guess -forever -loop -noxdamage -repeat -rfbport 5900 -shared -dontdisconnect -rfbauth /etc/vncpasswd -o /var/log/x11vnc.log
+ExecStart=/usr/bin/x11vnc -auth guess -forever -loop -noxdamage -repeat -rfbport ${PORT} -shared -dontdisconnect -rfbauth ${PASS} -o /var/log/x11vnc.log
 
 [Install]
 WantedBy=multi-user.target
-" > /lib/systemd/system/x11vnc.service
-x11vnc -storepasswd "$mypass" /etc/vncpasswd
-echo "включаем автозагрузку vncserver"
-systemctl enable --now x11vnc.service
-echo "удаленный доступ настроен"
-read
-fi
-if [ -n "$(command -v apt-get)" ]; then
-	echo "найден apt-get устанавливаем требуемые программы с помощью apt-get"
-  if [[ $EUID -ne 0 ]]; then
-     echo "введите пароль суперпользователя"
-     su -c 	"/bin/bash ./easyremotecontrol.sh"
-     exit 0
-  fi
-  if [[ $EUID -ne 0 ]]; then
-  echo "ошибка получения прав суперпользователя"
-  exit 1
-  fi
-echo "установка x11vnc"
-sudo apt-get install x11vnc -y
-x11vnc -storepasswd "$mypass" /etc/vncpasswd
-echo "
-[Unit]
-Description=Start x11vnc at startup.
-After=multi-user.target
+EOF
 
-[Service]
-Type=simple
-ExecStart=/usr/bin/x11vnc -auth guess -forever -loop -noxdamage -repeat -rfbport 5900 -shared -dontdisconnect -rfbauth /etc/vncpasswd -o /var/log/x11vnc.log
+  systemctl daemon-reload
+  systemctl enable --now x11vnc.service 1> /dev/null
+}
 
-[Install]
-WantedBy=multi-user.target
-" > /lib/systemd/system/x11vnc.service
-x11vnc -storepasswd "$mypass" /etc/vncpasswd
-echo "включаем автозагрузку vncserver"
-systemctl enable --now x11vnc.service
-echo "удаленный доступ настроен"
-read
-fi
+function final() {
+  printf "\n%s\n" "Настройка ${PACK}: [OK]"
+  exit 0
+}
 
+
+main
+install
+settings
+final
